d377d6deddc3b7a4feb2228d48e016885fd8bdec
jimenaaineto <jimena.ainetosalvador@usp.ceu.es>
2025-10-15 15:35:55 +0200

    Recetita guapa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,7 @@ public class Main {
         //omida: la crema de garbanzos es una forma muy sencilla y sabrosa de disfrutar de una buena ración de legumbres. Para continuar, proponemos unos espaguetis a la boloñesa, un clásico de la cocina casera que no puede estar más rico.
         //Cena: el revuelto de bacalao es un plato muy nutritivo y sencillo de preparar, estupendo para disfrutar de una cena rápida.
 
+
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
