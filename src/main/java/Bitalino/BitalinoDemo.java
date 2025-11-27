26c5346d87964795c164448af5bf136b817f7574
SaraMenor <sara.menortorrero@usp.ceu.es>
2025-11-27 14:54:01 +0100

    señales prelimpia

--- src/main/java/Bitalino/BitalinoDemo.java
+++ src/main/java/Bitalino/BitalinoDemo.java
@@ -21,7 +21,7 @@ public class BitalinoDemo {
         System.out.println("BITalino Connection");
 
         BITalino device = new BITalino();
-        String mac = UIUtils.readString("Enter BITalino MAC (Ej: 98:D3:41:FD:4E:E8): ");
+        String mac = UIUtils.readString("Enter BITalino MAC (Example: 98:D3:41:FD:4E:E8): ");
         mac = mac.trim();
 
         System.out.println("Connecting to BITalino at MAC: " + mac);
